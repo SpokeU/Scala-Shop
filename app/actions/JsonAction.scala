2578fc8ada6c3fe586e222bcc475d1c1b4bbf2c3
SpokeU <omyshko91@gmail.com>
2015-01-22 00:08:54 +0200

    Missing file

---
+++ app/actions/JsonAction.scala
@@ -0,0 +1,45 @@
+package actions
+
+import play.api.libs.json.Json.fromJson
+import play.api.mvc._
+import play.api.mvc.Request
+import play.api.mvc.Result
+import play.api.libs.json._
+import play.api.mvc.BodyParsers.parse
+import play.api.mvc.Results
+import play.api.mvc.Action
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import play.api.mvc.AnyContent
+import play.api.mvc.Results._
+
+object Actions {
+
+  def JsonModelAction[T](action: T => Result)(implicit parser: Reads[T]): Action[JsValue] = {
+    JsonModelAction[T]()(action)
+  }
+
+  def JsonModelAction[T](validators: (T => String)*)(action: T => Result)(implicit parser: Reads[T]): Action[JsValue] = {
+    Action(parse.json) {
+      implicit request =>
+        Json.fromJson(request.body) match {
+          case JsSuccess(model, _) => {
+            val errors = validateModel(model)(validators: _*)
+            errors match {
+              case Nil    => action(model)
+              case errors => BadRequest(Json.obj("errors" -> errors))
+            }
+          }
+          case JsError(e) => BadRequest(s"Invalid JSON format $e")
+        }
+    }
+  }
+
+  def validateModel[T](model: T)(validators: (T => String)*) = {
+    validators.foldLeft(List[String]())((acc, validator) => {
+      val validationResult = validator(model)
+      if (!validationResult.isEmpty()) validationResult :: acc else acc
+    })
+  }
+
+}
